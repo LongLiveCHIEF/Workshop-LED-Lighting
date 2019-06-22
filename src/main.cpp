9446b3b3a535c897ff6b51c164d0d431906333ce
Brian vanderbusch <brian.vanderbusch@gmail.com>
2019-06-22 16:49:35 -0500

    add fastled library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#define FASTLED_ESP_8266_RAW_PIN_ORDER 1
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define LED_DATA D1                         // Data pin to connect to the strip
+#define LED_DATA 1                         // Data pin to connect to the strip
 #define COLOR_ORDER RGB                     // Color Order for chip
 #define LED_TYPE WS2812B                    // LED Type
 #define NUM_LEDS 32*8                       // Number of LED's
