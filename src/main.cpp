44bc7031de4a13073b4bb0d5472f15213daf892c
Brian vanderbusch <brian.vanderbusch@gmail.com>
2019-06-22 16:01:40 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#define FASTLED_ESP_8266_RAW_PIN_ORDER 1
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_DATA D1                         // Data pin to connect to the strip
+#define COLOR_ORDER RGB                     // Color Order for chip
+#define LED_TYPE WS2812B                    // LED Type
+#define NUM_LEDS 32*8                       // Number of LED's
+
+uint8_t max_bright = 128;
+
+struct CRGB leds[NUM_LEDS];
+
+//
+
+void setup() {
+  delay(2000); // prevent crashy things
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  LEDS.addLeds<LED_TYPE, LED_DATA, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(max_bright);
+
+}
+
+void loop() {
+  fill_solid(leds, NUM_LEDS, CRGB::White);
+  FastLED.show();
+}
