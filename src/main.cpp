a232ea440656e970c5391d2ef83a0d681317f051
Brian vanderbusch <brian.vanderbusch@gmail.com>
2019-07-07 12:42:34 -0500

    updating to use extra params at captive portal

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
-#include <Arduino.h>
-#include <FastLED.h>
-#include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
+#include <FS.h>
 #include <WiFiManager.h>
-#include <Ticker.h>
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <FastLED.h>
 
 #define LED_DATA 1                         // Data pin to connect to the strip
 #define COLOR_ORDER RGB                     // Color Order for chip
 #define LED_TYPE WS2812B                    // LED Type
 #define NUM_LEDS 32*8                       // Number of LED's
+
+char mqtt_server[40];
+char mqtt_port[6];
+char mqtt_server_token[40];
+
+bool shouldSaveConfig = true;
 
 uint8_t max_bright = 128;
 
@@ -32,19 +36,61 @@ void configModeCallback (WiFiManager *myWiFiManager){
 
 void setup() {
   Serial.begin(115200);
+  Serial.println();
 
-  //set builtin led pin as output
-  pinMode(LED_BUILTIN, OUTPUT);
+  //clean FS, for testing
+  //SPIFFS.format();
 
-  // start ticker with 0.5 because we start in AP mode and try to connect
-  digitalWrite(LED_BUILTIN, LOW);
-  ticker.attach(0.6, tick);
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(blynk_token, json["blynk_token"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+
+  WiFiManager wifiManager;
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_server_token("token", "mqtt server token", mqtt_server_token, 40);
+
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_server_token);
 
   //Local Initialization
-  WiFiManager wifiManager;
   wifiManager.setAPCallback(configModeCallback);
 
-  if (!wifiManager.autoConnect()) {
+  if (!wifiManager.autoConnect("workshop-LED-AP")) {
     Serial.println("failed to connect and hit timeout");
     //reset and try again, or maybe put it to deep sleep
     ESP.reset();
@@ -53,6 +99,30 @@ void setup() {
 
   //if you got here, you're connected to WiFi
   Serial.println("connected to configured access point");
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_server_token, custom_mqtt_server_token.getValue());
+
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_server_token"] = mqtt_server_token;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+  }
+
   ticker.detach();
   //keep LED on
   digitalWrite(LED_BUILTIN, LOW);
